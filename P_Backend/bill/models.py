3a750b167979e6dc0f5331e34cd4fc1e8a024ddc
Andres <cesarandrestorresbernal604@gmail.com>
2023-07-05 22:25:37 -0500

    agregacion parking a settings

--- P_Backend/bill/models.py
+++ P_Backend/bill/models.py
@@ -2,14 +2,12 @@ from django.utils import timezone
 from django.db import models
 
 class Bill(models.Model):
-    id_Bill= models.AutoField(primary_key=True)
-    date_Entry = models.DateTimeField(default=timezone.now)
-    date_Exit = models.DateTimeField(default=timezone.now)
+    date_entry = models.DateTimeField(default=timezone.now)
+    date_exit = models.DateTimeField(default=timezone.now)
     zone= models.CharField(max_length=200,default='')
-    t_Contract= models.CharField(max_length=200,default='')
+    t_contract= models.CharField(max_length=200,default='')
 
 class Admin(models.Model):
-    id_Admin= models.AutoField(primary_key=True)
     user= models.CharField(max_length=200,default='')
     password= models.CharField(max_length=200,default='')
 
